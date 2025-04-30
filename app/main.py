fc157c6d544a9fab076ad92879914315bf5c5535
Leonardo Fonseca Rodrigues <leonardo.fonseca@tradexsolutions.com.br>
2025-04-30 11:14:36 -0300

    fix: cors updated

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ app = FastAPI(root_path="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://the-spirits-book-frontend.s3-website.us-east-2.amazonaws.com"
-    ],
+    allow_origins=["https://olivrodosespiritos.zhiloone.com.br/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
